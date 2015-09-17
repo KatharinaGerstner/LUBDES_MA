26df94f66db989b086ac3f1470b3c9f6f0c3eaa9
Michael Beckmann <michael.beckmann@ufz.de>
2015-09-17 08:51:05 +0200

    change data to normal dataframe

--- RScripts/-01_load_table_directly_from_google.R
+++ RScripts/-01_load_table_directly_from_google.R
@@ -1,6 +1,6 @@
-setwd("C:\\Users\\hoppek\\Documents\\GitHub\\LUBDES_MA") #KG
+#setwd("C:\\Users\\hoppek\\Documents\\GitHub\\LUBDES_MA") #KG
 
-install.packages("devtools")
+#install.packages("devtools")
 library(devtools)
 devtools::install_github("jennybc/googlesheets") # documentation at:http://htmlpreview.github.io/?https://raw.githubusercontent.com/jennybc/googlesheets/master/vignettes/basic-usage.html and https://github.com/jennybc/googlesheets
 library(googlesheets)
@@ -13,6 +13,7 @@ gs_auth(new_user = TRUE) # you get an unauthorized error
 
 LUBDES_gsheet<- gs_title("LUBDES coding table v2") # load LUBDES  coding table, this crashes sometimes but seems to work as of April 22 2015
 data <- gs_read(LUBDES_gsheet, ws = "1. Coding Table version 2") # consume data from sheet 1
+data<-as.data.frame(data)
 # data <- get_via_cf(LUBDES_gsheet, ws = "1. Coding Table version 2") #works but it seems to be confused by our first four lines # old function
 
 # ### OBSOLETE since empty rows have been deleted
