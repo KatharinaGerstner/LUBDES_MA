2e04e76fb379e7b2b033cf4c2631aaae7bb88a84
Katharina Gerstner <katharina.gerstner@ufz.de>
2015-09-18 10:46:34 +0200

    Analysis and Plotting work
    However, I had to restrict the analysis to cases with nonzero variances.
    Plot for no moderators is not yet working.

--- RScripts/02_DataAnalysis.R
+++ RScripts/02_DataAnalysis.R
@@ -1,38 +1,37 @@
 #ratio of ratios
 library(metafor)
-library(ggplot2)
 
 setwd("C:\\Users\\hoppek\\Documents\\GitHub\\LUBDES_MA") #KG
 #setwd("~/Dropbox/SESYNC-UFZ-sDiv-Call Biodiversity and Ecosystem Services/Meta-Analysis/DataAnalysis") #MB
 
 ES.frame = read.csv("Input/ES_table.csv")
-ES.frame <- ES.frame[-which(ES.frame$Study.ID=="4788-Mosquera-Losada2009"),] # Why?
+ES.frame.posVar <- ES.frame[ES.frame$Richness.Log.RR.Var>0 & ES.frame$Yield.Log.RR.Var>0,] # restrict analysis to study cases with positive variances
+ES.frame <- ES.frame.posVar
+ES.frame$LUI.range <- factor(ES.frame$LUI.range)
 
+### fit meta-analytic multivariate/multilevel fixed- and random/mixed-effects models with or without moderators via linear (mixed-effects) models using rma.mv
+## Viechtbauer 2015 p 189: the random argument can also contain one (and only one!) formula of the form ~ inner | outer . Effects or outcomes with different values/levels of the outer grouping variable/factor are assumed to be independent, while effects or outcomes with the same value/level of the outer grouping variable/factor share correlated random effects corresponding to the levels of the inner grouping variable/factor. The struct argument is used to specify the variance structure corresponding to the inner variable/factor. With struct="CS", a compound symmetric structure is assumed (i.e., a single variance component tau? corresponding to all values/levels of the inner variable/factor and a single correlation coefficient rho for the correlation between different values/levels). 
 ##############################################################################################################
 LMM.MA.fit <- function(yi,vi,mods,slab,inner2,outer2){
 ##############################################################################################################
-  ### fit meta-analytic multivariate/multilevel fixed- and random/mixed-effects models with or without moderators via linear (mixed-effects) models using rma.mv
-  ## Viechtbauer 2015 p 189: the random argument can also contain one (and only one!) formula of the form ~ inner | outer . Effects or outcomes with different values/levels of the outer grouping variable/factor are assumed to be independent, while effects or outcomes with the same value/level of the outer grouping variable/factor share correlated random effects corresponding to the levels of the inner grouping variable/factor. The struct argument is used to specify the variance structure corresponding to the inner variable/factor. With struct="CS", a compound symmetric structure is assumed (i.e., a single variance component tau? corresponding to all values/levels of the inner variable/factor and a single correlation coefficient rho for the correlation between different values/levels). 
-  # btw, there seems to be a problem with the terms inner and outer as they are also R base functions
 # TO DO: calculate variance-covariance matrix, esp covariance for between LUI comparisons 
 #  VCov_matrix <- 
-  rma.mv.fit <- rma.mv(yi=yi, V=vi, mods = mods, random = ~inner2|outer2, struct="CS", data=ES.frame, slab=slab,method="REML", tdist=FALSE, level=95, digits=4, Rscale="cor", sparse=FALSE, verbose=FALSE)
+  rma.mv.fit <- rma.mv(yi=yi, V=vi, mods = mods, random = ~factor(inner2)|factor(outer2), struct="CS", data=ES.frame, slab=slab,method="REML", tdist=FALSE, level=95, digits=4, Rscale="cor", sparse=FALSE, verbose=FALSE)
   return(rma.mv.fit)
 }
 
 # TO DO: make a list of moderators to be tested, store model results in a list, so they can be plotted in a next step (03_Plotting.r)
 ### Analysis without moderators
-mods <- 1
 attach(ES.frame)
-Richness.MA.fit <- LMM.MA.fit(yi=Richness.Log.RR, vi=Richness.Log.RR.Var, mods=~1, slab=paste(Study.Case, Low.LUI,High.LUI,sep="_"), inner2=1, outer2=Study.ID)
+Richness.MA.fit <- LMM.MA.fit(yi=Richness.Log.RR, vi=Richness.Log.RR.Var, mods=~1, slab=paste(Study.Case, Low.LUI,High.LUI,sep="_"), inner2=Case.ID, outer2=Study.ID)
 Yield.MA.fit <- LMM.MA.fit(yi=Yield.Log.RR,vi=Yield.Log.RR.Var,mods=~1,slab=paste(Study.Case,Low.LUI,High.LUI,sep="_"),inner=Study.Case,outer=Study.ID)
 detach(ES.frame)
 save(mods,Richness.MA.fit,Yield.MA.fit, file="Output/MA_model_1.Rdata")
 
 ### Store results in table
-MA.coeffs <- data.frame(Moderator="None",levels=1,number.of.studies.within.level=,mean.Richness=Richness.MA.fit$b,se.Richness=Richness.MA.fit$se,mean.Yield=Yield.MA.fit$b,se.Yield=Yield.MA.fit$se)
+MA.coeffs <- data.frame(Moderator="None",levels=1,mean.Richness=Richness.MA.fit$b,se.Richness=Richness.MA.fit$se,mean.Yield=Yield.MA.fit$b,se.Yield=Yield.MA.fit$se)
 
-moderator.list <- c("Habitat.Type","Species.Group","Trophic.Level","LUI.comparison","Study.Type")
+moderator.list <- c("Land.use...land.cover","Species.Group","Trophic.Level","LUI.range")
 
 for(mods in moderator.list){
   print(mods)
