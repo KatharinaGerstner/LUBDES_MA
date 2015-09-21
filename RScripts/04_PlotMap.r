053a6035668c57fedf09b88cbe8f93b88488c414
Michael Beckmann <michael.beckmann@ufz.de>
2015-09-21 14:22:28 +0200

    further files deleted

--- RScripts/04_PlotMap.r
+++
@@ -1,8 +0,0 @@
-###########
-### create map
-library(rworldmap)
-newmap <- getMap(resolution = "low")
-pdf("Output/map_of_studies.pdf",pointsize=8)
-plot(newmap, main="Distribution of LUBDES studies included in the meta analysis")
-points(data$lon, data$lat, col = "blue", cex = .6, pch=2)
-dev.off()
--- RScripts/10_Playground.r
+++
@@ -1,3 +0,0 @@
-## test the functionality of github first
-## Open R-scripts located in the local clone directory directly in R. Change it as you want and save it. 
-## Heee, I think I got it now!
