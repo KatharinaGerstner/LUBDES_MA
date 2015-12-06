663ae939ae9dbd4700c7ba75203f731f0bb4a8c5
Katharina Gerstner <katharina.gerstner@ufz.de>
2015-12-07 00:41:15 +0100

    write lonlat coordinates on the x- and y-axis

--- RScripts/08_Plotting.r
+++ RScripts/08_Plotting.r
@@ -26,12 +26,25 @@ getwd()
 # points(data$lon, data$lat, col = "blue", cex = .8, pch=17)
 # dev.off()
 # 
+
+world_map <- map_data("world")
+p <- ggplot() +
+  geom_polygon(data=world_map, aes(x=long, y=lat, group=group),fill="white",color="black",lwd=0.2) + 
+  geom_point(data=ES.frame, aes(x=Longitude, y=Latitude, ymin=-55), color="blue", shape=4, size=1) +
+  scale_x_continuous(breaks=c(-90, 0, 90), labels=c("90° W", "0", "90° E")) +
+  scale_y_continuous(breaks=c(-60, -30, 0, 30, 60), labels=c("60°S", "30°S","0°", "30°N","60°N")) +
+  xlab("") + ylab("") 
+p 
+ggsave(paste(path2temp, "/CaseDistributionAll.png",sep=""), width=18, height=10, units="cm")
+
 ### plot study locations using fancy ggplot
 world_map <- map_data("world")
 ES.frame <- within(ES.frame, LUI.range.level <- factor(LUI.range.level, levels = c("low-low","low-medium","low-high","medium-medium","medium-high","high-high"))) # resort levels for plotting
 p <- ggplot() +
   geom_polygon(data=world_map, aes(x=long, y=lat, group=group),fill="white",color="black",lwd=0.3) + 
   geom_point(data=ES.frame, aes(x=Longitude, y=Latitude, ymin=-55), color="blue", shape=2, size=1) +
+  scale_x_continuous(breaks=c(-90, 0, 90), labels=c("90° W", "0", "90° E")) +
+  scale_y_continuous(breaks=c(-60, -30, 0, 30, 60), labels=c("60°S", "30°S","0°", "30°N","60°N")) +
   xlab("") + ylab("") +
   facet_wrap(~LUI.range.level)   
 p 
